1677dcfec46cfc9c6010437e868de4d032fb8ce8
TrungDT <thanhtrung.it25@gmail.com>
2019-08-03 23:57:40 +0700

    Dynamic HTML Generation

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import HtmlWebpakPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -13,7 +14,13 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    // create HTML file that includes reference to bundle JS
+    new HtmlWebpakPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpakPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,6 +16,27 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+    // create HTML file that includes reference to bundle JS
+    new HtmlWebpakPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments:true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+
+      // Properties you define hereare available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: "3161d70dca1b48d9915015509a67e9a5"
+    }),
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     // Minify JS
